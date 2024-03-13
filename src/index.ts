184ee3e6670d0ed961fc4baab3c3b44a1e1006d5
Ian Becker <ian@beckr.dev>
2024-03-13 09:07:34 -0400

    fix(refresh): changes refresh token logic so that multiple can exist per user (e.g. for multi-device)

--- src/index.ts
+++ src/index.ts
@@ -92,15 +92,6 @@ async function getInactivePulicKey(db: D1Database): Promise<DatabasePubKey> {
 }
 
 async function generateRefreshToken(db: D1Database, userId: string): Promise<string> {
-	// First check to see if we already have a refresh token for this user
-	const existingToken = await db.prepare("SELECT token, expires_at FROM refresh_tokens WHERE user_id = ?").bind(userId).first() as { token: string, expires_at: Date } | undefined;
-	if (existingToken) {
-		if (existingToken.expires_at < new Date()) {
-			await db.prepare("DELETE FROM refresh_tokens WHERE token = ?").bind(existingToken.token).run();
-		} else {
-			return existingToken.token;
-		}
-	}
 	const tokenId = generateId(15);
 	const refreshToken = generateId(36);
 	await db.prepare("INSERT INTO refresh_tokens (id, token, user_id, expires_at) VALUES (?, ?, ?, DATETIME('now', '+30 days'))")
@@ -114,15 +105,17 @@ async function consumeRefreshToken(db: D1Database, token: string): Promise<void>
 }
 
 async function validateRefreshToken(db: D1Database, token: string): Promise<string | null> {
-	const refreshToken = await db.prepare("SELECT user_id FROM refresh_tokens WHERE token = ?").bind(token).first() as { user_id: string, expires_at: Date } | undefined;
+	const refreshToken = await db.prepare("SELECT user_id FROM refresh_tokens WHERE token = ? AND expires_at > DATETIME('now')")
+		.bind(token)
+		.first() as { user_id: string } | undefined;
+
 	if (refreshToken) {
-		if (refreshToken.expires_at < new Date()) {
-			await db.prepare("DELETE FROM refresh_tokens WHERE token = ?").bind(token).run();
-			return null;
-		}
-		return refreshToken.user_id;
+		return refreshToken.user_id; // Token is valid and not expired
+	} else {
+		// Token is invalid or expired; consider cleaning up expired tokens here or separately
+		await db.prepare("DELETE FROM refresh_tokens WHERE token = ?").bind(token).run();
+		return null;
 	}
-	return null;
 }
 
 const generateJWT = async (env: any, user: User): Promise<string> => {
