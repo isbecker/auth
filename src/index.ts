7baa3fec23e61048efd75e62dda51c6e404d097f
Ian Becker <ian@beckr.dev>
2024-03-13 12:12:30 -0400

    fix: adds auth/verify route

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { callbackHandler } from "@routes/auth/github/callback";
 import { loginHandler } from "@routes/auth/github/login";
 import { logoutHandler } from "@routes/auth/logout";
 import { refreshHandler } from "@routes/auth/refresh";
+import { verifyHandler } from "@routes/auth/verify";
 import { wellKnownJwksHandler, wellKnownOpenIDHandler } from "@routes/well-known";
 import { Env } from "@typing/env";
 import { Router } from "itty-router";
@@ -20,25 +21,28 @@ export default {
 		const github = initializeGitHub(env);
 		const lucia = initializeLucia(env.DB);
 
-		router.get("/auth/github/login", async (request: Request, env: Env, ctx: ExecutionContext) => {
+		router.get('/auth/github/login', async (request: Request, env: Env, ctx: ExecutionContext) => {
 			return await loginHandler(request, github, lucia);
 		});
-		router.get("/auth/github/callback", async (request: Request, env: Env, ctx: ExecutionContext) => {
+		router.get('/auth/github/callback', async (request: Request, env: Env, ctx: ExecutionContext) => {
 			return await callbackHandler(request, github, lucia, env);
 		});
-		router.get("/auth/logout", async (request: Request, env: Env, ctx: ExecutionContext) => {
+		router.get('/auth/logout', async (request: Request, env: Env, ctx: ExecutionContext) => {
 			return await logoutHandler(request, lucia);
 		});
 		router.get('/auth/refresh', async (request: Request, env: Env, ctx: ExecutionContext) => {
 			return await refreshHandler(request, lucia, env);
 		});
+		router.get('/auth/verify', async (request: Request, env: Env, ctx: ExecutionContext) => {
+			return verifyHandler(request, lucia, env);
+		});
 		router.get('/.well-known/openid-configuration', async (request: Request, env: Env, ctx: ExecutionContext) => {
 			return await wellKnownOpenIDHandler(request, env);
 		});
-		router.get("/.well-known/jwks.json", async (request: Request, env: Env, ctx: ExecutionContext) => {
+		router.get('/.well-known/jwks.json', async (request: Request, env: Env, ctx: ExecutionContext) => {
 			return await wellKnownJwksHandler(request, env);
 		});
-		router.all("*", async (request: Request, env: Env, ctx: ExecutionContext) => {
+		router.all('*', async (request: Request, env: Env, ctx: ExecutionContext) => {
 			return new Response(null, {
 				status: 404,
 			});
